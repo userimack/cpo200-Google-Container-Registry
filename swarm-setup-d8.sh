3af530aef2d505661e1680a8056361c1dee2b6a0
Mahendra <mahendra.k12@gmail.com>
2018-10-09 12:54:20 +0530

    modified swarm-setup-d8.sh setup script

--- swarm-setup-d8.sh
+++ swarm-setup-d8.sh
@@ -22,13 +22,17 @@ cd
 echo 'Installing Docker...'
 sudo apt-get purge lxc-docker*
 sudo apt-get purge docker.io*
+sudo apt-get remove docker docker-engine docker.io
 sudo apt-get update
-sudo apt-get -y -qq install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo bash -c "echo deb https://apt.dockerproject.org/repo debian-jessie main >> /etc/apt/sources.list.d/docker.list"
+sudo apt-get -y -qq install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+# sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+# sudo bash -c "echo deb https://apt.dockerproject.org/repo debian-jessie main >> /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
-sudo apt-cache policy docker-engine
-sudo apt-get -y -qq install docker-engine
+# sudo apt-cache policy docker-ce
+sudo apt-get -y -qq install docker-ce
 sudo service docker start
 sudo docker run hello-world
 sudo gpasswd -a $USER docker
